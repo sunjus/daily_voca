66a4415a90075c066fa454d8bd76d6a6b12853a2
sunjus <sunju.shin1108@gmail.com>
2021-04-13 01:04:03 +0300

    Rewrited code to API request

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,22 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import data from "../db/data.json";
 import { useParams } from "react-router-dom";
 import Word from "./Word";
 
 const Day = () => {
+  const [words, setWords] = useState([]);
   //useParams(): parameters on a given URL (App.js day/:day)
   //const { day } = useParams();
   const day = useParams().day;
-  const dailyWord = data.words.filter((word) => word.day === Number(day));
-  console.log(data);
-  console.log(dailyWord);
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/words?day=${day}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setWords(data);
+      });
+    //의존성 배열dependency array
+  }, [day]);
 
   return (
     <div className="word">
       <h2>DAY {day}</h2>
       <table>
         <tbody>
-          {dailyWord.map((word) => (
+          {words.map((word) => (
             <Word word={word} key={word.id} />
           ))}
         </tbody>
--- src/component/DayList.js
+++ src/component/DayList.js
@@ -3,24 +3,24 @@ import { Link } from "react-router-dom";
 
 const DayList = () => {
   const [days, setDays] = useState([]);
-  const [count, setCount] = useState(1);
-
-  const countPlus = () => {
-    setCount(count + 1);
-  };
 
   useEffect(() => {
-    console.log("changed");
-  });
+    fetch("http://localhost:3001/days")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setDays(data);
+      });
+  }, []);
 
   return (
-    <ul>
+    <ul className="list_day">
       {days.map((day) => (
         <li key={day.id} className="list_day">
           <Link to={`/day/${day.day}`}>DAY{day.day}</Link>
         </li>
       ))}
-      <button onClick={countPlus}>{count}</button>
     </ul>
   );
 };
