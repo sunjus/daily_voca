0b647c570fb90135581702f14c13ed53d30103ce
sunjus <sunju.shin1108@gmail.com>
2021-04-14 00:46:59 +0300

    Added useRef hook to access DOM

--- src/component/CreateWord.js
+++ src/component/CreateWord.js
@@ -1,23 +1,35 @@
-import React from "react";
+import React, { useRef } from "react";
 import useFetch from "../hooks/useFetch";
 
 const CreateWord = () => {
   const days = useFetch("http://localhost:3001/days");
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(engRef.current.value);
+    console.log(defRef.current.value);
+    console.log(dayRef.current.value);
+  };
+
+  //to access DOM eg.focus,scroll
+  const engRef = useRef(null);
+  const defRef = useRef(null);
+  const dayRef = useRef(null);
+
   return (
     <div>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="input_area">
           <label> English </label>
-          <input type="text" placeholder="computer" />
+          <input type="text" placeholder="computer" ref={engRef} />
         </div>
         <div className="input_area">
           <label> Definition </label>
-          <input type="text" placeholder="컴" />
+          <input type="text" placeholder="컴" ref={defRef} />
         </div>
         <div className="input_area">
           <label>Day</label>
-          <select>
+          <select ref={dayRef}>
             {days.map((day) => (
               <option key={day.id} value={day.day}>
                 {day.day}
