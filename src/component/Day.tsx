a9e426bcc52202cf7883ae12b45aab7275c441e2
sunjus <sunju.shin1108@gmail.com>
2021-04-20 00:16:53 +0300

    Added buttons to next day

--- src/component/Day.tsx
+++ src/component/Day.tsx
@@ -1,31 +1,39 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
+import { useState } from "react"
 import Word, { IWord } from "./Word";
+import { IDay } from "./DayList"
 import useFetch from "../hooks/useFetch";
 
 const Day = () => {
   //useParams(): parameters on a given URL (App.js day/:day)
   //const { day } = useParams();
   const day = useParams<{ day : string }>().day;
+  const days : IDay[] = useFetch("http://localhost:3001/days");
   const words : IWord[] = useFetch(`http://localhost:3001/words?day=${day}`);
+  console.log(day)
+  console.log(words)
+  const current = Number(day)
+  console.log(current)
+/*
+  const length = days.length
+  console.log(length)
+  
+  function prevDay(){
+   return(current === 1 ? length-1 : current-1) 
+  }
 
-  /*
-  useEffect(() => {
-    fetch(`http://localhost:3001/words?day=${day}`)
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        setWords(data);
-      });
-    //의존성 배열dependency array: this day = line 14 {day}
-  }, [day]);
-  */
-
+  const nextDay = () => 
+    Number(day) === length-1 ? 1 : Number(day)+1;
+*/  
+  
   return (
     <div className="word">
-      {words.length === 0 && <span>Loading...</span>}
-      <h2>DAY {day}</h2>
+      {words.length === 0 && <span>Day Loading...</span>}
+      <div className="word_header">
+        <Link to={`/day/${Number(day)-1}`}><span>◀️</span></Link>             
+        <h2>DAY {day}</h2>   
+        <Link to={`/day/${Number(day)+1}`}><span>▶️</span></Link>             
+      </div>
       <table>
         <tbody>
           {words.map((word) => (
